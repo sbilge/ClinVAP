ea22ef1f2dd4370878fbc53b59f378f1dd80a632
Bilge Surun <bilges@Bilges-MacBook-Pro.local>
2018-08-16 12:48:44 +0200

    First commit

---
+++ VepFileDeployment/README.md
@@ -0,0 +1,2 @@
+# Clinical Reporting Application - File Deployment
+This is a docker container for downloading and deploying the dependecy files of Ensembl-VEP (release 93) used in the clinical reporting application. 
---
+++ VepFileDeployment/copy_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -e /mnt/homo_sapiens ]; then
+    cp -r /opt/vep/.vep/homo_sapiens /mnt
+fi
+
+for file in human_ancestor.fa.rz human_ancestor.fa.rz.fai LoFtool_scores.txt phylocsf.sql; do
+    if  [ ! -f /mnt/$file ]; then
+        cp $file /mnt
+    fi
+done
+wait
+touch /mnt/completeness.flag
+echo "Dependency files are now in the volume. Ready to start ClinicalReportR service" >> /mnt/complete.flag
