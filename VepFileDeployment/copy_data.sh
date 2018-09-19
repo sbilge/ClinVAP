2c8ad10ef120925601ca5becb19db8c34011af7e
Bilge Surun <bilges@Bilges-MacBook-Pro.local>
2018-09-19 14:38:31 +0200

    cache files, vep user settings

--- VepFileDeployment/copy_data.sh
+++ VepFileDeployment/copy_data.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-if [ -f docker.flag ]; then
-    if [ ! -e /mnt/homo_sapiens ]; then
-        cp -r /opt/vep/.vep/homo_sapiens /mnt
-    fi
-
-    for file in human_ancestor.fa.rz human_ancestor.fa.rz.fai LoFtool_scores.txt phylocsf.sql; do
-        if  [ ! -f /mnt/$file ]; then
-            cp $file /mnt
-        fi
-    done
-else
-    cd /mnt
-    su -c "perl /opt/vep/src/ensembl-vep/INSTALL.pl -n -c /mnt --CACHE_VERSION 93 --VERSION 93 -a acf -s homo_sapiens -y GRCh37" vep
-    wget http://www.broadinstitute.org/~konradk/loftee/human_ancestor.fa.rz
-    wget http://www.broadinstitute.org/~konradk/loftee/human_ancestor.fa.rz.fai
-    wget https://raw.githubusercontent.com/Ensembl/VEP_plugins/release/90/LoFtool_scores.txt
-    wget https://www.broadinstitute.org/%7Ekonradk/loftee/phylocsf.sql.gz && \
-    gunzip phylocsf.sql.gz
+if [ ! -e /mnt/homo_sapiens ]; then
+    cp -r /opt/vep/homo_sapiens /mnt
 fi
+
+for file in human_ancestor.fa.rz human_ancestor.fa.rz.fai LoFtool_scores.txt phylocsf.sql; do
+    if  [ ! -f /mnt/$file ]; then
+        cp $file /mnt
+    fi
+done
+
 wait
 touch /mnt/completeness.flag
 echo "Dependency files are now in the volume. Ready to start ClinicalReportR service" >> /mnt/complete.flag
