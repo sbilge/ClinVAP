1f7bcd54432433db804a7d7bc84cfc84c5a21324
Bilge Surun <bilges@whatever.Informatik.Uni-Tuebingen.De>
2018-09-20 12:54:26 +0200

    fasta files download

--- VepFileDeployment/copy_data.sh
+++ VepFileDeployment/copy_data.sh
@@ -12,7 +12,8 @@ if [ -f docker.flag ]; then
     done
 else
     cd /mnt
-    perl /opt/vep/src/ensembl-vep/INSTALL.pl -n -c /mnt -d /mnt --CACHE_VERSION 93 --VERSION 93 -a acf -s homo_sapiens -y GRCh37
+    perl /opt/vep/src/ensembl-vep/INSTALL.pl -n -c /mnt -d /mnt --CACHE_VERSION 93 --VERSION 93 -a acf -s homo_sapiens -y GRCh37 &&\
+    cp /opt/vep/.vep/homo_sapiens /mnt
     wget http://www.broadinstitute.org/~konradk/loftee/human_ancestor.fa.rz
     wget http://www.broadinstitute.org/~konradk/loftee/human_ancestor.fa.rz.fai
     wget https://raw.githubusercontent.com/Ensembl/VEP_plugins/release/90/LoFtool_scores.txt
