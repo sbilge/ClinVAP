51caf0d3336ebfff5b4de9cd70ab60303f72e5cb
Bilge Surun <bilges@whatever.Informatik.Uni-Tuebingen.De>
2019-02-25 11:21:10 +0100

    /tmp cleanup

--- ReportingApplication/make_report.sh
+++ ReportingApplication/make_report.sh
@@ -18,6 +18,7 @@ shift "$((OPTIND-1))"
 CPWD=$(pwd)
 RESULTS="${inputFolder}/results"
 LOGS="${inputFolder}/results/logs"
+TMP="${inputFolder}/results/tmp"
 
 for file in ${inputFolder}/*.vcf;
 do
@@ -25,10 +26,11 @@ outfilename=$(basename "$file")
 outname="${outfilename%.*}"
 
 mkdir -p $LOGS
+mkdir $TMP
 
 # annotate file
 echo "################ Starting variant effect prediction ################"
-vep -i $file -o /tmp/$outname.vcf --config /opt/vep/.vep/vep.ini 
+vep -i $file -o $TMP/$outname.vcf --config /opt/vep/.vep/vep.ini 
 
 # check if there are any metadata for patient info
 metadata="${outname}"_metadata.json
@@ -37,31 +39,31 @@ metadata="${outname}"_metadata.json
 echo "################ Start to create json ################"
 if [ -f docker.flag ]; then # called by docker
     if [ ! -f $inputFolder/$metadata ]; then
-        Rscript /opt/vep/reporting.R -f /tmp/$outname.vcf -r /tmp/$outname.json
+        Rscript /opt/vep/reporting.R -f $TMP/$outname.vcf -r $TMP/$outname.json
     else
-        Rscript /opt/vep/reporting.R -f /tmp/$outname.vcf -r /tmp/$outname.json -m $inputFolder/$metadata
+        Rscript /opt/vep/reporting.R -f $TMP/$outname.vcf -r $TMP/$outname.json -m $inputFolder/$metadata
     fi
 else
     if [ ! -f $inputFolder/$metadata ]; then
-        Rscript /opt/vep/reporting.R -f /tmp/$outname.vcf -r /tmp/$outname.json -d /opt/vep/driver_db_dump.json
+        Rscript /opt/vep/reporting.R -f $TMP/$outname.vcf -r $TMP/$outname.json -d /opt/vep/driver_db_dump.json
     else
-        Rscript /opt/vep/reporting.R -f /tmp/$outname.vcf -r /tmp/$outname.json -d /opt/vep/driver_db_dump.json -m $inputFolder/$metadata
+        Rscript /opt/vep/reporting.R -f $TMP/$outname.vcf -r $TMP/$outname.json -d /opt/vep/driver_db_dump.json -m $inputFolder/$metadata
     fi
 fi
 
 if [[ $savedOut == *"j"* ]]; then
-    cp /tmp/$outname.json $RESULTS
+    cp $TMP/$outname.json $RESULTS
     if [ -f $RESULTS/$outname.json ]; then
         echo "JSON is saved to the volume"
     fi
 fi
 
-mv /tmp/*.log $LOGS
+mv $TMP/*.log $LOGS
 
 echo "################ Start to create report ################"
-nodejs /opt/vep/clinicalreporting_docxtemplater/main.js -d /tmp/$outname.json -t /opt/vep/clinicalreporting_docxtemplater/data/template.docx -o /tmp/$outname.docx
+nodejs /opt/vep/clinicalreporting_docxtemplater/main.js -d $TMP/$outname.json -t /opt/vep/clinicalreporting_docxtemplater/data/template.docx -o $TMP/$outname.docx
 if [[ $savedOut == *"w"* ]]; then
-    cp /tmp/$outname.docx  $RESULTS
+    cp $TMP/$outname.docx  $RESULTS
     if [ -f $RESULTS/$outname.docx ]; then
         echo "DOCX is saved to the volume"
     fi
@@ -69,11 +71,12 @@ fi
 
     # convert it to pdf
 if [[ $savedOut == *"p"* ]]; then
-    libreoffice --headless --convert-to pdf /tmp/$outname.docx  --outdir  $RESULTS
+    libreoffice --headless --convert-to pdf $TMP/$outname.docx  --outdir  $RESULTS
     if [ -f $RESULTS/$outname.pdf ]; then
         echo "PDF is saved to the volume"
     fi
 fi
 # clean tmp directory
-find /tmp -type f -amin +30 -delete
+find $TMP -type f -amin +30 -delete
 done
+rm -rf $TMP
